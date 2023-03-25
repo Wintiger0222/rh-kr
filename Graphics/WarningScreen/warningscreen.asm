693384d3f45b6d814ba7eca9993e1a2744033463
Wintiger0222 <0xodbs02@naver.com>
2023-03-25 11:56:12 +0900

    Fix warning screen

--- Graphics/WarningScreen/warningscreen.asm
+++ Graphics/WarningScreen/warningscreen.asm
@@ -5,5 +5,5 @@
 .org 0x08D1B00C+4
 .dw D1AD4C_warningscreen_tile_map_rle
 ;.org 0x08D1B00C+8
-;.dh D1AD4C_warningscreen_tile_map_rle-D1AD4C_warningscreen_tile_map_rle_end
+.dh D1AD4C_warningscreen_tile_map_rle_end-D1AD4C_warningscreen_tile_map_rle
 ;eof
